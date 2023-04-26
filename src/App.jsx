01d266f47682aa0d37e03688b4fe931ae6d87d3d
Suvrajit Mondal <rdxarijitmondal.com>
2023-04-26 18:49:54 +0530

    Hero section complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ const App = () =>(
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`styles.boxWidth`}>
+    <div className={` ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
         <Hero/>
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`styles.boxWidth`}>
+    <div className={` ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
         <Stats/>
         <Business/>
         <Billing/>
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { arrowUp } from '../assets';
+import styles from '../style';
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () =>(
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] bg-blue-gradient rounded-full p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}>
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
+          <span className='text-gradient'>Get</span>
+        </p>
+        <img src={arrowUp} alt="arrow" className='w-[23px] h-[23px]'/>
+      </div>
+      <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+        <span className='text-gradient'>Started</span>
+      </p>
+    </div>
+  </div>
+)
+
 
 export default GetStarted
